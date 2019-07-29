4e416ef350148e87435c9885f63505aabe5f94a8
Patrick Roos <pa.roos@icloud.com>
2019-07-29 18:00:58 +0200

    add tech radar data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-console.log('Your code here.');
+let techRadar = {
+	"Meta": {
+	  "generatedAt": new Date()
+	},
+	"TechRadar": {
+	  "Techniques": ["Micro Frontends", "Polyglot programming", "Secrets as a service", "Chaos Engineering"],
+	  "Tools": ["Cypress", "Helm", "Traefik", "Humio"],
+	  "Platforms": ["Contentful", "AWS Fargate", "InfluxDB"],
+	  "Languages & Frameworks": ["TypeScript", "Rust"]
+  }
+};
